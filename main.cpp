0ccdebf814434c334f40c059610496aa67af7259
7shi <7shi@live.jp>
2013-06-05 17:04:02 +0900

    オペコードにレジスタが含まれている命令

--- main.cpp
+++ main.cpp
@@ -4,6 +4,10 @@
 #include <vector>
 #include <string>
 
+std::string regs8 [] = { "al", "cl", "dl", "bl", "ah", "ch", "dh", "bh" };
+std::string regs16[] = { "ax", "cx", "dx", "bx", "sp", "bp", "si", "di" };
+std::string sregs [] = { "es", "cs", "ss", "ds" };
+
 struct OpCode {
 	size_t len;
 	std::string mne, op1, op2;
@@ -34,6 +38,7 @@ OpCode disasm(uint8_t *p) {
 	case 0x3f: return OpCode(1, "aas");
 	case 0x98: return OpCode(1, "cbw");
 	case 0x99: return OpCode(1, "cwd");
+	case 0x90: return OpCode(1, "nop");
 	case 0x9b: return OpCode(1, "wait");
 	case 0x9c: return OpCode(1, "pushf");
 	case 0x9d: return OpCode(1, "popf");
@@ -70,6 +75,19 @@ OpCode disasm(uint8_t *p) {
 	case 0xfb: return OpCode(1, "sti");
 	case 0xfc: return OpCode(1, "cld");
 	case 0xfd: return OpCode(1, "std");
+	}
+	switch (p[0] & ~7) {
+	case 0x40: return OpCode(1, "inc" , regs16[p[0] & 7]);
+	case 0x48: return OpCode(1, "dec" , regs16[p[0] & 7]);
+	case 0x50: return OpCode(1, "push", regs16[p[0] & 7]);
+	case 0x58: return OpCode(1, "pop" , regs16[p[0] & 7]);
+	case 0x90: return OpCode(1, "xchg", regs16[p[0] & 7], "ax");
+	}
+	if (p[0] != 0x0f) {
+		switch (p[0] & 0xe7) {
+			case 6: return OpCode(1, "push", sregs[(p[0] >> 3) & 7]);
+			case 7: return OpCode(1, "pop" , sregs[(p[0] >> 3) & 7]);
+		}
 	}
 	undefined++;
 	return OpCode(1, "(undefined)");
