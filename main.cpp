f31ca4209f57738c1f55af113e8ce4b3f6ddfaad
7shi <7shi@live.jp>
2013-06-10 21:31:13 +0900

    レジスタマクロを追加

--- main.cpp
+++ main.cpp
@@ -7,7 +7,22 @@ uint16_t ip, r[8];
 uint8_t *r8[8];
 uint8_t text[65536], mem[65536], *data;
 
+#define AX r[0]
+#define CX r[1]
+#define DX r[2]
+#define BX r[3]
 #define SP r[4]
+#define BP r[5]
+#define SI r[6]
+#define DI r[7]
+#define AL *r8[0]
+#define CL *r8[1]
+#define DL *r8[2]
+#define BL *r8[3]
+#define AH *r8[4]
+#define CH *r8[5]
+#define DH *r8[6]
+#define BH *r8[7]
 
 static void debug() {
 	fprintf(stderr,
