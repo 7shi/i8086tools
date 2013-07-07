adb0e25edf77d5d591bc7fb31eb2e018735a2911
7shi <7shi@live.jp>
2013-07-08 07:56:22 +0900

    [V6]VMのメンバ関数をMinix2に合わせる

--- 7run/UnixV6/VM.h
+++ 7run/UnixV6/VM.h
@@ -2,16 +2,27 @@
 #include "../PDP11/VM.h"
 
 namespace UnixV6 {
+
     class VM : public PDP11::VM {
     public:
         VM();
+        VM(const VM &vm);
         virtual ~VM();
-        virtual void setstat(uint16_t addr, struct stat *st);
+
+    protected:
         virtual bool syscall(int n);
-        virtual int convsig(int sig);
         virtual void setsig(int sig, int h);
-        
+        virtual void setstat(uint16_t addr, struct stat *st);
+
     private:
         bool syscall(int n, uint8_t *args);
+        int v6_fork(); //  2
+        int v6_exec(); // 11
+        int v6_signal(); // 48
+
+        static void sighandler(int sig);
+        void sighandler2(int sig);
+        virtual int convsig(int sig);
+        void resetsig();
     };
 }
