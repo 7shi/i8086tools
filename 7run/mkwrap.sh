2a0a3c83337d15f22d00590b6a91d612f7475e19
7shi <7shi@live.jp>
2013-07-09 14:32:05 +0900

    [V6]ラッパーコマンドを追加 - 接頭辞なしラッパーを7binに格納

--- 7run/mkwrap.sh
+++ 7run/mkwrap.sh
@@ -1,9 +1,18 @@
+# sh mkwrap.sh /usr/local/bin/7run /usr/local/minix2 /usr/local/bin/m2 usr/bin/cc
+
+# ex. /usr/local/bin/7run
 run=$1
+
+# ex. /usr/local/minix2
 root=$2
+
+# ex. /usr/local/bin/m2cc
 cmd=$3`basename $4`
-m2cmd=$2/usr/$4
+
+# ex. /usr/local/minix2/usr/bin/cc
+wrap=$2/$4
 
 echo "#!/bin/sh" > $cmd
-echo "exec $run -r $root $m2cmd \$@" >> $cmd
+echo "exec $run -r $root $wrap \$@" >> $cmd
 
 chmod 755 $cmd
