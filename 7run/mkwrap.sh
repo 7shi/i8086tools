f8a8307a892d4c1ef54ed82fa21f43a556a8d8ca
7shi <7shi@live.jp>
2013-07-16 15:09:44 +0900

    ラッパーにデバッグオプションを追加 (--s, --v, --m)

--- 7run/mkwrap.sh
+++ 7run/mkwrap.sh
@@ -19,13 +19,20 @@ cmd=$3`basename $4`
 # ex. /usr/local/minix2/usr/bin/cc
 wrap=$2/$4
 
-echo "#!/bin/sh" > $cmd
+cat << EOT > $cmd
+#!/bin/sh
+opt=""
+if [ "x\$1" = "x--s" ]; then opt="-s"; shift; fi
+if [ "x\$1" = "x--v" ]; then opt="-v"; shift; fi
+if [ "x\$1" = "x--m" ]; then opt="-m"; shift; fi
+EOT
+
 if [ "x$opt" = "x-x" ]
 then
-    echo "$run -r $root $wrap \$@" >> $cmd
+    echo "$run \$opt -r $root $wrap \$@" >> $cmd
     echo "exit 0" >> $cmd
 else
-    echo "exec $run -r $root $wrap \$@" >> $cmd
+    echo "exec $run \$opt -r $root $wrap \$@" >> $cmd
 fi
 
 chmod 755 $cmd
