92490e4f9e446175fc4e4cfadd024cf4f77412a4
7shi <7shi@live.jp>
2013-07-23 10:02:00 +0900

    [8086V6]putc, puts, strlen

--- libc/cret.s
+++ libc/cret.s
@@ -1,5 +1,18 @@
-.extern .cret
+.extern .cret, .sret, .dsret
 .cret:
 	mov sp, bp
+	pop bp
+	ret
+
+.sret:
+	lea sp, -2(bp)
+	pop si
+	pop bp
+	ret
+
+.dsret:
+	lea sp, -4(bp)
+	pop di
+	pop si
 	pop bp
 	ret
