0af410e2c74a0a38f3138b50d7a4e07d83ba5709
7shi <7shi@live.jp>
2013-08-14 14:22:34 +0900

    バイトオーダーを判定して処理を最適化

--- 7run/utils.h
+++ 7run/utils.h
@@ -4,6 +4,23 @@
 
 extern std::string rootpath;
 
+#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+inline uint16_t read16(uint8_t *mem) {
+    return *(uint16_t *)mem;
+}
+
+inline uint32_t read32(uint8_t *mem) {
+    return *(uint32_t *)mem;
+}
+
+inline void write16(uint8_t *mem, uint16_t v) {
+    *(uint16_t *)mem = v;
+}
+
+inline void write32(uint8_t *mem, uint32_t v) {
+    *(uint32_t *)mem = v;
+}
+#else
 inline uint16_t read16(uint8_t *mem) {
     return mem[0] | (mem[1] << 8);
 }
@@ -23,6 +40,7 @@ inline void write32(uint8_t *mem, uint32_t v) {
     mem[2] = v >> 16;
     mem[3] = v >> 24;
 }
+#endif
 
 std::string readstr(uint8_t *mem, int max);
 
