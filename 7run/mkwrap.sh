a1de0b9508682b8f8cbb0475b8c40a170990a2bf
7shi <7shi@live.jp>
2015-11-20 10:56:43 +0900

    [V7]ツールチェインをバイナリ配布

--- 7run/mkwrap.sh
+++ 7run/mkwrap.sh
@@ -19,6 +19,8 @@ cmd=$3`basename $4`
 # ex. /usr/local/minix2/usr/bin/cc
 wrap=$2/$4
 
+shift; shift; shift; shift
+
 cat << EOT > $cmd
 #!/bin/sh
 opt=""
@@ -29,7 +31,7 @@ EOT
 
 if [ "x$opt" = "x-x" ]
 then
-    echo "$run \$opt -r $root $wrap \$@" >> $cmd
+    echo "$run \$opt $@ -r $root $wrap \$@" >> $cmd
     echo "exit 0" >> $cmd
 else
     echo "exec $run \$opt -r $root $wrap \$@" >> $cmd
